ecedb861ac4326169892cc95deb63785e0214b93
Michael <git@h9h.de>
2022-12-12 20:12:54 +0100

    Day 12

--- 11/Day11.hs
+++ 11/Day11.hs
@@ -1,6 +1,124 @@
 module Day11 where
-  
+
+import Data.List (sort)
+
 solve :: IO ()
-solve = do 
-  raw <- readFile "11/input.txt"
-  print raw
+solve = do
+  -- raw <- readFile "11/input.txt"
+  -- print raw
+  let inputMonkeys = initializeMonkeys
+  let inputItems = initializeItems
+  print $ take 21 $ iterate (playRound1 inputMonkeys) (replicate (length inputMonkeys) 0, inputItems)
+  let result = last $ take 21 $ iterate (playRound1 inputMonkeys) (replicate (length inputMonkeys) 0, inputItems)
+  let counts = reverse $ sort $ fst result
+  print counts
+  print $ head counts * counts !! 1
+  let result2 = last $ take 10001 $ iterate (playRound2 inputMonkeys) (replicate (length inputMonkeys) 0, inputItems)
+  let counts2 = reverse $ sort $ fst result2
+  print $ head counts2 * counts2 !! 1
+
+type Item = (Int, Integer)
+type Items = [Item]
+type Operation = Integer -> Integer
+type Test = Integer -> Bool
+type Targets = (Int, Int)
+
+data Monkey = Monkey {
+    key :: Int,
+    operation :: Operation,
+    predicate :: Test,
+    targets :: Targets
+  }
+
+type Monkeys = [Monkey]
+
+initializeTestMonkeys :: Monkeys
+initializeTestMonkeys = [
+    Monkey 0 (* 19)        (\n -> n `mod` 23 == 0) (2,3),
+    Monkey 1 (+ 6)         (\n -> n `mod` 19 == 0) (2,0),
+    Monkey 2 (\n -> n * n) (\n -> n `mod` 13 == 0) (1,3),
+    Monkey 3 (+ 3)         (\n -> n `mod` 17 == 0) (0,1)
+  ]
+
+initializeTestItems :: Items
+initializeTestItems = [
+    (0, 79), (0, 98),
+    (1, 54), (1, 65), (1, 75), (1, 74),
+    (2, 79), (2, 60), (2, 97),
+    (3, 74)
+  ]
+
+initializeMonkeys :: Monkeys
+initializeMonkeys = [
+    Monkey 0 (* 5)         (\n -> n `mod`  7 == 0) (6,7),
+    Monkey 1 (+ 1)         (\n -> n `mod` 17 == 0) (0,6),
+    Monkey 2 (+ 8)         (\n -> n `mod` 11 == 0) (5,3),
+    Monkey 3 (+ 4)         (\n -> n `mod` 13 == 0) (0,1),
+    Monkey 4 (+ 7)         (\n -> n `mod` 19 == 0) (5,2),
+    Monkey 5 (+ 2)         even                    (1,3),
+    Monkey 6 (* 11)        (\n -> n `mod`  5 == 0) (7,4),
+    Monkey 7 (\n -> n * n) (\n -> n `mod`  3 == 0) (4,2)
+  ]
+
+productDivisors :: Integer
+productDivisors = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19
+
+initializeItems :: Items
+initializeItems = [
+    (0,89),(0,84),(0,88),(0,78),(0,70),
+    (1,76),(1,62),(1,61),(1,54),(1,69),(1,60),(1,85),
+    (2,83),(2,89),(2,53),
+    (3,95),(3,94),(3,85),(3,57),
+    (4,82),(4,98),
+    (5,69),
+    (6,82),(6,70),(6,58),(6,87),(6,59),(6,99),(6,92),(6,65),
+    (7,91),(7,53),(7,96),(7,98),(7,68),(7,82)
+  ]
+
+applyPart1 :: Monkey -> Item -> Item
+applyPart1 m item = (newMonkey, value)
+  where value = operation m (snd item) `div` 3
+        newMonkey = if predicate m value then fst (targets m) else snd (targets m)
+
+applyPart2 :: Monkey -> Item -> Item
+applyPart2 m item = (newMonkey, value)
+  where value = operation m (snd item) `mod` productDivisors
+        newMonkey = if predicate m value then fst (targets m) else snd (targets m)
+
+{-
+>>> applyPart1 (initializeTestMonkeys !! 0) (0, 79) 
+(3,500)
+-}
+
+
+playMonkey :: (Monkey -> Item -> Item) -> Monkey -> ([Int], Items) -> ([Int], Items)
+playMonkey f m (c, items) = (count, new ++ old)
+  where k = key m
+        new = f m <$> filter (\(j, _) -> j == k) items
+        old = filter (\(j, _) -> j /= k) items
+        count = map (\i -> c !! i + if i == k then length new else 0) [0..length c - 1]
+
+{-
+>>> playMonkey applyPart1 (initializeTestMonkeys !! 0) ([0,5,0,0], initializeTestItems)
+([8,5,0,0],[(3,500),(3,620),(1,54),(1,65),(1,75),(1,74),(2,79),(2,60),(2,97),(3,74)])
+-}
+
+seq' :: [a -> a] -> a -> [a]
+seq' fs z = scanl (\x f -> f x) z fs
+
+playRound1 :: Monkeys -> ([Int], Items) -> ([Int], Items)
+playRound1 ms = last . seq' (playMonkey applyPart1 <$> ms)
+
+playRound2 :: Monkeys -> ([Int], Items) -> ([Int], Items)
+playRound2 ms = last . seq' (playMonkey applyPart2 <$> ms)
+
+{-
+>>> playRound1 initializeTestMonkeys ([0,0,0,0], initializeTestItems)
+([2,4,3,5],[(1,401),(1,1046),(1,167),(1,207),(1,25),(1,2080),(0,20),(0,23),(0,27),(0,26)])
+
+>>> last $ take 21 $ iterate (playRound1 initializeTestMonkeys) ([0,0,0,0], initializeTestItems)
+([101,95,7,105],[(1,115),(0,34),(1,199),(1,53),(1,93),(1,245),(0,26),(0,14),(0,12),(0,10)])
+
+>>> last $ take 10001 $ iterate (playRound2 initializeTestMonkeys) ([0,0,0,0], initializeTestItems)
+([50695,49301,661,50640],[(1,9201133),(0,949756),(1,4050898),(1,312078),(1,690729),(1,1166945),(1,6216613),(0,667213),(0,4587084),(0,1153594)])
+-}
--- 12/Day12.hs
+++ 12/Day12.hs
@@ -1,6 +1,95 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day12 where
-  
+
+import Algorithm.Search (dijkstra)
+import Data.Char (ord)
+import Data.List (elemIndices, intercalate)
+import Data.Maybe (fromMaybe)
+import Control.Arrow ((&&&))
+
+type Position = (Int, Int)
+type Terrain = [String]
+type Cost  = Int
+
+output :: Maybe (Int, [(Int, Int)]) -> String
+output x = case x of
+  Just (n, _) -> show n
+  Nothing     -> error "fail"
+
 solve :: IO ()
-solve = do 
+solve = do
   raw <- readFile "12/input.txt"
-  print raw
+  let terrain = lines raw
+  print $ getStart terrain
+  print $ getValue terrain $ getStart terrain
+  print $ neighbors terrain $ getStart terrain
+  print $ "Part 1           : " ++ output (solveShortestPath terrain (getStart terrain))
+  print $ "Part 2           : " ++ show (findShortestHike terrain)
+
+getPos :: Char -> Terrain -> Position
+getPos char' t = (r,c)
+  where r = fst . head . filter (\(_, s) -> char' `elem` s) $ zip [0..] t
+        c = fst . head . filter (\(_, s) -> char' == s) $ zip [0..] (t !! r)
+
+getStart :: Terrain -> Position
+getStart = getPos 'S'
+
+getValue :: Terrain -> Position -> Char
+getValue t (r,c) 
+  | r < 0 || r > length t - 1         = '.'
+  | c < 0 || c > length (head t) - 1  = '.' 
+  | otherwise                         = t !! r !! c
+
+neighbors :: Terrain -> Position -> [Position]
+neighbors t (x, y) = filter (\p -> let v = getValue t p in v /= '.' && accessible v v') [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+  where v' = getValue t (x, y)
+
+accessible :: Char -> Char -> Bool
+accessible v   'S' = accessible v  'a'
+accessible 'E' v'  = accessible 'z' v'
+accessible v   v'  = ord v - ord v' <= 1
+
+{-
+>>> accessible 'b' 'a'
+True
+-}
+
+cost :: Terrain -> Position -> Position -> Cost
+cost _ _ _ = 1
+
+finished :: Terrain -> Position -> Bool
+finished t (r,c) = 'E' == t !! r !! c
+
+solveShortestPath :: Terrain -> Position -> Maybe (Cost, [Position])
+solveShortestPath t = dijkstra (neighbors t) (cost t) (finished t)
+
+{-
+>>> solveShortestPath (lines "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi") (0,0)
+Just (31,[(0,1),(1,1),(2,1),(2,2),(3,2),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(3,7),(2,7),(1,7),(0,7),(0,6),(0,5),(0,4),(0,3),(1,3),(2,3),(3,3),(3,4),(3,5),(3,6),(2,6),(1,6),(1,5),(1,4),(2,4),(2,5)])
+-}
+
+findAllStarts :: Terrain -> [Position]
+findAllStarts t = findPositions . map (\c -> if c == 'S' then 'a' else c) $ intercalate "" t
+  where findPositions s = map (\p -> (p `div` n, p `mod` n)) $ elemIndices 'a' s
+        n = length $ head t
+
+{-
+>>> findAllStarts (lines "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi")
+[(0,0),(0,1),(1,0),(2,0),(3,0),(4,0)]
+
+>>> elemIndices 'a' "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi"
+[1,9,18,27,36]
+-}
+
+findShortestHike :: Terrain -> Cost
+findShortestHike =  
+      fst 
+        . minimum 
+        . map (fromMaybe (maxBound, []))
+        . uncurry map
+        . (solveShortestPath &&& findAllStarts)
+
+{-
+>>> findShortestHike (lines "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi")
+29
+-}
